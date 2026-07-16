ee5c05c8def30e444799c4b93979c3c8b91ef315
LAPTOP-UUD3H92N\Consucorp <fernando@consucorp.com.mx>
2026-07-15 19:00:19 -0600

    Creacion del Proyecto y primeros avances con la arquitectura creada y servicio y funcionamineto basico creado tambien

---
+++ app/Models/Services/Pokemon/PokemonNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Core.  Backend creado por CONSUCORP para Editorial Santillana.
+ * Copyright (c) 2026.
+ * fernando@consucorp.com.mx
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ *
+ */
+
+namespace App\Models\Services\Pokemon;
+
+class PokemonNotFoundException extends \Exception
+{
+    protected $message = 'Pokemon not found';
+}
